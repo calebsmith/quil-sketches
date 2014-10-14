2754730cfeb4bf97614264b7b5ae0a2655c5d20b
Caleb Smith <caleb.smithnc@gmail.com>
2014-10-14 18:41:05 -0400

    Add logic for GOL o quil_sketches sketch

--- src/quil_sketches/core.clj
+++ src/quil_sketches/core.clj
@@ -3,9 +3,68 @@
   (:require [quil.core :refer :all])
   (:require [quil.middleware :as m]))
 
-(defsketch gen-art-21
+; Start with Gosper Gun
+(def base-states [
+  [23 16]
+  [24 16]
+  [22 17]
+  [21 18]
+  [21 19]
+  [21 20]
+  [26 17]
+  [27 18]
+  [27 19]
+  [27 20]
+  [28 19]
+  [25 19]
+  [26 21]
+  [22 21]
+  [23 22]
+  [24 22]
+  [35 14]
+  [35 15]
+  [33 15]
+  [32 16]
+  [31 16]
+  [32 17]
+  [31 17]
+  [32 18]
+  [32 18]
+  [33 19]
+  [35 19]
+  [35 20]
+  [45 16]
+  [46 16]
+  [45 17]
+  [46 17]
+  [11 18]
+  [12 18]
+  [11 19]
+  [12 19]
+])
+
+
+(defn base-states->initial-states [states]
+  (vec (flatten (map (fn [[x y]]
+      [(+ (* y dynamic/board-width) x) 1]
+  ) states))))
+
+
+(defn build-initial-board [states]
+  (let [default-board (vec (repeat (* dynamic/board-width dynamic/board-height) 0))]
+    (apply assoc (cons default-board (base-states->initial-states states)))))
+
+
+(defn setup []
+    (smooth)
+    (background 255)
+    (frame-rate 60)
+    {:board (build-initial-board base-states)})
+
+
+(defsketch qsketch
   :title "new stuff"
-  :setup dynamic/setup
+  :setup setup
   :draw dynamic/draw
   :update dynamic/update
   :middleware [m/fun-mode]
--- src/quil_sketches/dynamic.clj
+++ src/quil_sketches/dynamic.clj
@@ -1,28 +1,68 @@
 (ns quil-sketches.dynamic
   (:require [quil.core :refer :all]))
 
-(def board-width 100)
-(def board-height 100)
 
-(defn setup []
-  (smooth)
-  (background 255)
-  (frame-rate 60)
-  {:board [0 1 1 0]})
-
-(defn draw-cell [x y]
-  (ellipse x y 5 5))
+(def board-width 80)
+(def board-height 80)
 
 
-(defn draw-board []
-  (map (fn [y] (println y)) (range board-height)))
+(defn board-get-index [x y]
+    (+ (* y board-width) x))
+
+
+(defn board-get-cell [board xin yin]
+  (let [x (mod xin board-width)
+        y (mod yin board-height)]
+    (nth board (board-get-index x y))))
+
+
+(defn draw-cell [board x y]
+  (let [state (board-get-cell board x y)]
+    (if (= state 1)
+      (ellipse (* 10 x) (* 10 y) 4 4))))
+
+
+(defn draw-board [board]
+  (doall (map (fn [y]
+    (doall (map (fn [x]
+      (draw-cell board x y))
+      (range board-width))))
+    (range board-height))))
 
 
 (defn draw [state]
   (background 255)
-  (draw-cell 10 10)
-  (draw-board))
+  (draw-board (:board state)))
+
+
+(defn board-count-neighbor [board x y]
+  (reduce + (map (fn [x-offset y-offset]
+      (board-get-cell board (+ x x-offset) (+ y y-offset)))
+    [-1 0 1 -1 1 -1 0 1]
+    [-1 -1 -1 0 0 1 1 1])))
+
+
+(defn board-next-cell [current neighbors]
+    (if (= current 0)
+        (if (= neighbors 3)
+            1
+            0)
+        (if (or (= neighbors 2) (= neighbors 3))
+            1
+            0)))
+
+
+(defn generate [board]
+  (vec (flatten (map (fn [y]
+    (doall (map (fn [x]
+      (let [num-neighbors (board-count-neighbor board x y)
+            current (board-get-cell board x y)]
+        (board-next-cell current num-neighbors)))
+      (range board-width))))
+    (range board-height)))))
 
 
 (defn update [state]
-  state)
+  (let [board (:board state)
+        next-board (generate board)]
+    {:board next-board}))
